07b5a18ce9ccf0ab02e551eda62a6ace153c941f
Didac T <didactor91@gmail.com>
2026-04-22 22:44:38 +0200

    fix: add urql Provider to wrap App
    The urql Client was created but never provided to the React tree,
    causing 'executeQuery is not a function' errors because hooks like
    useQuery need to be called within a Provider context

--- cfs/client/src/main.tsx
+++ cfs/client/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'urql'
+import { graphqlClient } from './graphql/client'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider value={graphqlClient}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
